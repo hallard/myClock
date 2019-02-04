f03fb003ffe6f1b6be57706505d28bb6602a74d9
Charles <hallard04@free.fr>
2019-02-04 22:54:50 +0100

    Sync from master

--- build.sh
+++ build.sh
@@ -6,32 +6,47 @@
 APP="myClock"
 SRC="$APP.ino"
 BIN="$SRC.bin"
+RM=`which rm`
 
 #default board d1_mini
 board="esp8266com:esp8266:d1_mini:xtal=160,vt=flash,eesz=4M1M,ip=lm2f,dbg=Disabled,lvl=NoAssert-NDEBUG,wipe=none,baud=921600"
+port="8266"
 
-while getopts ":lvf:h" opt; do
+while getopts ":lvf:hcw" opt; do
 	case $opt in
 		l)
-			board="esp8266com:esp8266:d1_mini_lite:xtal=160,vt=flash,eesz=1M64,ip=lm2f,dbg=Disabled,lvl=NoAssert-NDEBUG,wipe=none,baud=921600";;
+			echo "Board lolin32 selected." >&2
+			board="espressif:esp32:lolin32:PartitionScheme=min_spiffs"
+			port="3232"
+			;;
 		v)
 			verbose="--verbose"
-			debug="--debug";;
+			debug="--debug"
+			;;
 		f)
-			FLASH=$OPTARG;;
+			FLASH=$OPTARG
+			echo "Uploading to $FLASH after build." >&2
+			;;
 		h)
-			echo "Usage: build.sh [-b board] [-f IP] [-h]"
+			echo "Usage: build.sh [-l] [-v] [-f IP] [-h] [-c] [-w]"
 			echo ""
 			echo "Build ${APP} and optionally flash device."
 			echo ""
 			echo "Options:"
-			echo -e "\t-l\tBuild for d1_mini_lite instead of d1_mini"
+			echo -e "\t-l\tBuild for lolin32 instead of d1_mini"
 			echo -e "\t-v\tUse verbose output"
 			echo -e "\t-f\tIP address of ${APP} to flash update"
 			echo -e "\t-h\tDisplay usage and exit"
+			echo -e "\t-c\tClean build directory before building"
+			echo -e "\t-w\tWipe entire flash, instead of sketch only"
 			exit 0;;
+		c)
+			clean=true;;
+		w)
+			echo "Wipe entire flash on upload." >&2
+			board=${board/wipe=none/wipe=all};;
 		\?)
-			echo "invalid option -$OPTARG" >&2
+			echo "Invalid option -$OPTARG" >&2
 			exit 1;;
 		:)
 			echo "Option -$OPTARG requires an argument." >&2
@@ -55,24 +70,27 @@ elif [[ "$OSTYPE" == "msys" ]]; then
 else
 	arduino="`which arduino`"
 	if [ -z "$arduino" ]; then
-		arduino=`find ~/.local /usr -type f -name arduino -print -quit`
+		arduino="`find ~/.local /usr -type f -name arduino -print -quit`"
 	fi
 	buildpath="$HOME/.build"
-	espota="$HOME/Arduino/hardware/esp8266com/esp8266/tools/espota.py"
+	espota="`find ~/Arduino/hardware/esp8266com ~/Documents ~/.local /usr -type f -name espota.py -print -quit`"
 fi
 
 if [ -z "$arduino" ]; then
-	echo "Arduino IDE must be installed"
+	echo "Arduino IDE must be installed" >&2
 	exit 1
 else
-	echo "Building ${APP} in ${buildpath}..."
+	if [ "$clean" = true ]; then
+		echo "Cleaning ${buildpath}..." >&2
+		"${RM}" ${verbose} --one-file-system -rf "${buildpath}"
+	fi
+	echo "Building ${APP} in ${buildpath}..." >&2
 	"${arduino}" ${verbose} --pref build.path="${buildpath}" --board "${board}" --save-prefs --verify ${SRC}
 	ret=$?
 	if [ $ret -eq 0 ] && [ -f "$espota" ] && [ ! -z "$FLASH" ]; then
-		echo "Flashing ${BIN} to ${FLASH}..."
-		"${espota}" ${debug} --progress --file="${buildpath}/${BIN}" --ip=${FLASH}
+		echo "Flashing ${BIN} to ${FLASH}..." >&2
+		"${espota}" ${debug} --progress --file="${buildpath}/${BIN}" --ip=${FLASH} --port=${port}
 	else
 		exit $ret
 	fi
 fi
-
