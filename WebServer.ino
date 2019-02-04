f45701a79e7f68118941845c73cc9f0154171888
Charles <hallard04@free.fr>
2019-02-04 15:04:23 +0100

    Added some features

--- WebServer.ino
+++ WebServer.ino
@@ -1,45 +1,49 @@
 // Web Server for configuration and updates
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
+#ifdef ESP32
+  #include <WiFi.h>        //https://github.com/esp8266/Arduino
+  #include <ESPmDNS.h>
+  #include <AsyncTCP.h>
+#else
+ #include <ESP8266WiFi.h>        //https://github.com/esp8266/Arduino
+ #include <ESP8266mDNS.h>
+ #include <Hash.h>
+ #include <ESPAsyncTCP.h>
+#endif
+
 #include <ArduinoOTA.h>
 #include <FS.h>
-#include <Hash.h>
-#include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFSEditor.h>
 
-#define ADMIN_USER "admin"
-
 AsyncWebServer server(80);
-
 
 void handleOptions(AsyncWebServerRequest *request)
 {
   String response = "";
   int ret ;
 
-  Serial.println(PSTR("Serving handleOptions"));
+  OUT.println(PSTR("Serving handleOptions"));
   
   // Cant't use PSTR CFG_SAVE here, crash dump hasArg don't have _P
   if ( request->hasArg( "myColor") ) {
     uint8_t params = request->params();
     int i, l;
     char buff[128]; 
-    Serial.printf_P(PSTR("Form with %d parameters\r\n"), params);
+    OUT.printf_P(PSTR("Form with %d parameters\r\n"), params);
 
     // Navigate for all args, and simulate as it was typed from command line
     for ( i = 0; i < params; i++ ) {
       AsyncWebParameter* param = request->getParam(i);
       // We are in async, try to work fast
       // less print, less time 
-      Serial.printf_P(PSTR("[%02d] %s %s\r\n"), i, param->name().c_str(), param->value().c_str());
+      OUT.printf_P(PSTR("[%02d] %s %s\r\n"), i, param->name().c_str(), param->value().c_str());
 
       if (param->name() == "myColor") {
         myColor = htmlColor565(param->value());
       }
       if (param->name() == "brightness") {
         l = param->value().toInt();
-        brightness = brightness ? brightness : 255;
+        brightness = l ? l : 255;
       }
 
       if (param->name() == "threshold") {
@@ -60,18 +64,19 @@ void handleOptions(AsyncWebServerRequest *request)
       if (param->name() == "softAPpass") softAPpass = param->value();
       if (param->name() == "APpass") APpass = param->value();
       if (param->name() == "APname") APname = param->value();
+      if (param->name() == "HTTPpass") HTTPpass = param->value();
       if (param->name() == "location") location = param->value();
       
       if (param->name() == "timezone")  {
         String c = param->value();
-        if (c != "") {
+        //if (c != "") {
           c.trim();
           c.replace(' ', '_');
           if (timezone != c) {
             timezone = c;
-            setNTP(timezone);
+            setNTPNow();
           }
-        }
+        //}
       }
     } // For all params
 
@@ -83,34 +88,43 @@ void handleOptions(AsyncWebServerRequest *request)
     // now we can save the config
     if ( writeSPIFFS() ) {
       ret = 200;
-      response = PSTR("OK");
+      response = PSTR("<!DOCTYPE html><html><head><title>Configuration Saved</title>" 
+                      "<meta http-equiv='refresh' content='2; URL=/'>" 
+                      "</head><body><h2>Configuration saved, refreshing page in 2s</h2>"
+                      "</body></html>");
     } else {
-      ret = 412;
       response = PSTR("Error while saving configuration");
     }
 
   } else  {
     ret = 400;
-    response = PSTR("Missing Form Field");
+    response = PSTR("Error Missing Form Field");
   }
 
-  request->send ( ret, "text/plain", response);
+  request->send ( ret, "text/html", response);
 }
 
 void handleReset(AsyncWebServerRequest * request)
 {
-  AsyncResponseStream *response = request->beginResponseStream("text/html");
-  Serial.println(F("webServer: reset"));
+  String response = "";
+  OUT.println(F("webServer: reset"));
 
-  response->printf("<!DOCTYPE html><html><head><title>Webpage at %s</title></head><body>", request->url().c_str());
-  response->print("<h2>Restarting<h2>");
-  response->print("</body></html>");
-  request->send(response);
+  response = PSTR("<!DOCTYPE html><html><head><title>Reseting clock</title>"
+                  "<meta http-equiv='refresh' content='5; URL=/index.htm'></head><body>"
+                  "<h2>Restarting, refreshing page in 5s<h2>"
+                  "</body></html>");
 
-  delay(100);
-  ESP.reset();
+  request->send( 200, "text/html", response);
+  delay(250);
+  ESP.restart();
   // Should never arrive there
-  while (true);
+  //while (true);
+}
+
+void handleLogout(AsyncWebServerRequest * request)
+{
+  OUT.println(F("webServer: logout"));
+  request->send(401);
 }
 
 void handleNotFound(AsyncWebServerRequest * request)
@@ -176,6 +190,10 @@ String template_processor(const String& var)
   if (var == "language") return String(language);
   if (var == "milTime" && milTime) return F("checked");
   if (var == "celsius" && celsius) return F("checked");
+  if (var == "light") return String(light);
+  if (var == "threshold_h") return String((threshold<<1));
+  if (var == "threshold_l") return String(threshold);
+  if (var == "threshold") return String(threshold);
 
   if(var == "date") {
     time_t now = time(nullptr);
@@ -184,14 +202,17 @@ String template_processor(const String& var)
     return t;
   }
 
-  // class defined in myclock.css
-  if(var == "ds_class") {
-    #ifdef DS18
-      return F("ds_show");
-    #else
-      return F("ds_hide");
-    #endif
-  }
+#ifdef DS18
+  if (var=="ds_class") return F("div_show");
+#else
+  if (var=="ds_class") return F("div_hide");
+#endif
+
+#ifdef LIGHT
+  if(var == "lg_class") return F("div_show");
+#else
+  if(var == "lg_class") return F("div__hide");
+#endif
 
   if(var == "myColor") {
     char c[8];
@@ -203,9 +224,14 @@ String template_processor(const String& var)
   return String();
 }
 
-void startWebServer() {
 
-  server.addHandler(new SPIFFSEditor(ADMIN_USER, softAPpass));
+void startWebServer(const char* http_username, const char* http_password) {
+  
+  #ifdef ESP32
+  server.addHandler(new SPIFFSEditor(SPIFFS, http_username, http_password));
+  #else
+  server.addHandler(new SPIFFSEditor(http_username, http_password));
+  #endif
 
   server.on("/heap", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(200, "text/plain", String(ESP.getFreeHeap()));
@@ -213,26 +239,29 @@ void startWebServer() {
 
   server.on("/reset", handleReset);
   server.on("/options", handleOptions);
+  server.on("/logout", handleLogout);
+
   server.onNotFound(handleNotFound);
 
-  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.htm").setTemplateProcessor(template_processor);
+  server.serveStatic("/", SPIFFS, "/")
+        .setDefaultFile("index.htm")
+        .setTemplateProcessor(template_processor)
+        .setAuthentication(http_username, http_password);
 
-  //server.serveStatic("/", SPIFFS, "/www/").setTemplateProcessor(processor);
-  
   server.onFileUpload([](AsyncWebServerRequest *request, const String& filename, size_t index, uint8_t *data, size_t len, bool final){
     if(!index)
-      Serial.printf("UploadStart: %s\n", filename.c_str());
-    Serial.printf("%s", (const char*)data);
+      OUT.printf("UploadStart: %s\n", filename.c_str());
+    OUT.printf("%s", (const char*)data);
     if(final)
-      Serial.printf("UploadEnd: %s (%u)\n", filename.c_str(), index+len);
+      OUT.printf("UploadEnd: %s (%u)\n", filename.c_str(), index+len);
   });
 
   server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){
     if(!index)
-      Serial.printf("BodyStart: %u\n", total);
-    Serial.printf("%s", (const char*)data);
+      OUT.printf("BodyStart: %u\n", total);
+    OUT.printf("%s", (const char*)data);
     if(index + len == total)
-      Serial.printf("BodyEnd: %u\n", total);
+      OUT.printf("BodyEnd: %u\n", total);
   });
 
   // Start the Server
