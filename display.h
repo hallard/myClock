df78d33be7df81b421f6e0dc034aec6957c10283
Charles <hallard04@free.fr>
2019-02-04 22:42:57 +0100

    Added 64x64 Display and ESP32 compatibility

--- display.h
+++ display.h
@@ -5,7 +5,10 @@
 
 #include <Arduino.h>
 
-#define double_buffer
+//#define double_buffer
+//#define DISPLAY_32x64
+#define DISPLAY_64x64
+
 #include <PxMatrix.h> // https://github.com/2dom/PxMatrix
 #include <gfxfont.h>
 #include "fonts/TomThumb.h"
@@ -13,6 +16,7 @@
 #include <Ticker.h>
 Ticker display_ticker;
 
+#if defined(ESP8266)
 #define P_LAT 16
 #define P_A 5
 #define P_B 4
@@ -20,8 +24,22 @@ Ticker display_ticker;
 #define P_D 12
 #define P_E 0   // not needed on 64x32
 #define P_OE 2
+#else
+#define P_LAT 26
+#define P_A 22
+#define P_B 21
+#define P_C 5
+#define P_D 19
+#define P_E 17  // not needed on 64x32
+#define P_OE 16
 
+#endif
+
+#ifdef DISPLAY_64x64
+PxMATRIX display(64, 64, P_LAT, P_OE, P_A, P_B, P_C, P_D, P_E);
+#else
 PxMATRIX display(64, 32, P_LAT, P_OE, P_A, P_B, P_C, P_D);
+#endif
 
 uint16_t htmlColor565(const String htmlColor) {
   long c = strtol(htmlColor.substring(1).c_str(), NULL, 16);
@@ -41,15 +59,50 @@ uint32_t color565to888(uint16_t c) {
   return ((r << 16) | (g << 8) | b);
 }
 
-void display_updater() {
+#if defined (ESP8266)
+// ISR for display refresh
+void display_updater()
+{
   display.display(70);
 }
 
+#elif defined (ESP32)
+
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR display_updater() 
+{
+  portENTER_CRITICAL_ISR(&timerMux);
+  display.display(15);
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
+#endif
+
+
 #include "Digit.h"
+#ifdef DISPLAY_64x64
+#ifdef ESP8266
+const byte row1 = 6;
+const byte row2 = 14;
+const byte row3 = 54;
+const byte row4 = 62;
+#else
+const byte row1 = 6;
+const byte row2 = 14;
+const byte row3 = 22;
+const byte row4 = 30;
+const byte row5 = 38;
+const byte row6 = 46;
+const byte row7 = 54;
+const byte row8 = 62;
+#endif
+#else
 const byte row1 = 6;
 const byte row2 = 14;
 const byte row3 = 22;
 const byte row4 = 31;
+#endif
+
 const uint16_t myRED = display.color565(255, 0, 0);
 const uint16_t myGREEN = display.color565(0, 255, 0);
 const uint16_t myBLUE = display.color565(0, 0, 255);
